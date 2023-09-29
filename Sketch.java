0de38c843cf0856e8c48905bc86591de0930d7b8
brianau12 <brian.au25@ycdsbk12.ca>
2023-09-29 09:59:14 -0400

    diff sizes

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(200, 200);
   }
 
   /** 
@@ -27,31 +27,31 @@ public class Sketch extends PApplet {
 	// sample code, delete this stuff
     stroke(0);  
     fill(268, 168, 52);
-    rect((float) width/4, (float) height/2, (float) width/2, (float) height/(400/150));
-    triangle(width/2, height/4, (float) width/(400/300), (float) height/2, (float) width/4, (float) height/2);
+    rect((float) (width/4), (float) (height/2), (float) (width/2), (float) (height/(400.0/150.0)));
+    triangle((width/2), (height/4), (float) (width/(400.0/300.0)), (float) (height/2), (float) (width/4), (float) (height/2));
 
     stroke(0);
     fill(255, 255, 255);
-    rect((float) width/(400/163), (float) height/(400/260), (float) width/(400/75), (float) height/(400/90));
-    line((float) width/2, (float) height/(400/260), (float) width/2, (float) height/(400/350));
+    rect((float) (width/(400.0/163.0)), (float) (height/(400.0/260.0)), (float) (width/(400.0/75.0)), (float) (height/(400.0/90.0)));
+    line((float) (width/2), (float) (height/(400.0/260.0)), (float) (width/2), (float) (height/(400.0/350.0)));
 
     stroke(0);
     fill(200, 200, 200);
-    ellipse((float) width/(400/207), (float) height/(400/310), (float) width/(400/7), (float) height/(400/7)); 
+    ellipse((float) (width/(400.0/207.0)), (float) (height/(400.0/310.0)), (float) (width/(400.0/7)), (float) (height/(400.0/7))); 
 
     stroke(0);
     fill(40, 176, 59);
-    rect((float) 0, (float) height/(400/350), (float) width, (float) height/4);
+    rect((float) (0), (float) (height/(400.0/350.0)), (float) (width), (float) (height/4));
 
     stroke(0);
     fill(240, 252, 61);
-    ellipse((float) width/(400/330), (float) height/(400/60), (float) width/8, (float) height/8);
+    ellipse((float) (width/(400.0/330.0)), (float) (height/(400.0/60.0)), (float) (width/8), (float) (height/8));
 
     stroke(0);
     fill(255, 255, 255);
-    rect((float) width/(400/177), (float) height/(400/139), (float) width/(80/9), (float) height/8);
-    line((float) width/(400/177), (float) height/(80/33), (float) width/(200/111), (float) height/(80/33));
-    line((float)  width/2, (float) height/(20/7), (float) width/2, (float) height/(100/47));
+    rect((float) (width/(400.0/177.0)), (float) (height/(400.0/139.0)), (float) (width/(80.0/9)), (float) (height/8));
+    line((float) (width/(400.0/177.0)), (float) (height/(80.0/33.0)), (float) (width/(200.0/111.0)), (float) (height/(80.0/33.0)));
+    line((float)  (width/2), (float) (height/(20.0/7)), (float) (width/2), (float) (height/(100.0/47.0)));
   }
   
   // define other methods down here.
